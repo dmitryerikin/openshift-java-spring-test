795713b15cd2b70b087443cfaf66ab51744e99ab
Ерикин Дмитрий Валерьевич <Erikin.D.V@sberbank.ru>
2020-05-27 18:10:23 +0300

    initial commit

---
+++ src/main/java/io/dmitryerikin/openshiftjavaspringtest/OpenshiftJavaSpringTestApplication.java
@@ -0,0 +1,13 @@
+package io.dmitryerikin.openshiftjavaspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftJavaSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftJavaSpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dmitryerikin/openshiftjavaspringtest/OpenshiftJavaSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dmitryerikin.openshiftjavaspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftJavaSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
